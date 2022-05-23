13a9d3787ed1b7d80a532cc0c98c3aab39d17d3a
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2022-05-23 18:21:57 +1000

    index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all flats
+  root to: "flats#index"
+  # See information on specific flat
+  get "flats/:id", to: "flats#show", as: "flat"
 end
